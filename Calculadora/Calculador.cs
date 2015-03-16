7aeda3d1bb3e0cb106174a2029ff56ab178db47b
zappknight <victorfernandes.nave@gmail.com>
2015-03-16 11:05:53 -0300

    Iteratividade e recursividade
    Adicionamos o calculo de fatorial e fibonacci através dos métodos de recursividade e iteratividade.

--- Calculadora/Calculador.cs
+++ Calculadora/Calculador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,53 @@ namespace Calculadora
 {
     class Calculador
     {
+        public double FatorialIterativa(double n)
+        {
+            double result = 1;
 
+            for (int i = 1; i <= n; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        //recursiva - chama ela mesma
+        public double FibonacciRecursiva(double num)
+        {
+            if (num == 1)
+                return 0;
+            else if (num == 2)
+                return 1;
+            else
+                return FibonacciRecursiva(num - 1) + FibonacciRecursiva(num - 2);
+        }
+
+        public double FibonacciIterativa(double num)
+        {
+
+            double a = 0;
+            double b = 1;
+            double c = 1;
+
+            for (double i = 1; i < num; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return a;
+        }
+
+
+        public double FatorialRecursiva(double n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+                return n * FatorialRecursiva(n - 1);
+        }
         public double Soma(double n1, double n2)
         {
             return n1 + n2;
@@ -28,14 +74,8 @@ namespace Calculadora
 
         public double Potencia(double g1, double g2)
         {
-            double resultPot = g1;
-            for (double i = 1; i < g2; i++)
-            {
-
-                resultPot *= g1;
-
-            }
-            return resultPot;
+            
+            return Math.Pow(g2,g1);
         }
 
         public double Raiz(double s1, double s2)
@@ -55,17 +95,17 @@ namespace Calculadora
 
         public double Sen(double s1)
         {
-            return Math.Sin(s1);
+            return Math.Sin(s1* (Math.PI/ 180)); 
         }
 
         public double Cos(double s1)
         {
-            return Math.Cos(s1);
+            return Math.Cos(s1 * (Math.PI / 180));
         }
 
         public double Tg(double s1)
         {
-            return Math.Tan(s1);
+            return Math.Tan(s1 * (Math.PI / 180));
         }
     }
 }
